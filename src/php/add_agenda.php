1f5c5153325054ddefd324fc1509581dc5f51232
RafaelSantos1234 <rafa_gc123@hotmail.com>
2021-04-17 21:15:13 -0300

    separei o css

--- src/php/add_agenda.php
+++ src/php/add_agenda.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../global.css">
+    <link rel="stylesheet" href="../css/add_quadros.css">
     <script src="../js/jquery-3.5.1.min.js"></script>
     <script src="../js/arquivo.js"></script>
     <title>Tela principal</title>
