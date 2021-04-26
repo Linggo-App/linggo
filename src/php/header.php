6eca85e02e5187937cdd3d1225f5aecd508b88a7
Valdoveste <nickyv.oliveira@gmail.com>
2021-04-26 19:00:38 -0300

    titutlo e mini-logo adicionado.

--- src/php/header.php
+++ src/php/header.php
@@ -3,13 +3,15 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <title>Linggo</title>
+    <link rel="icon" href="../../assets/Mini-Logo.png">
+    <!-- <meta http-equiv="refresh" content="1" >  -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../global.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;700&display=swap" rel="stylesheet">
-    <title>Linggo</title>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
