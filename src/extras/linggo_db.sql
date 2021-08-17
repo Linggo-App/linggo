10c9542df52c56a3a4c76f9c575465036871afd0
Nicky Valdoveste <nickyv.oliveira@gmail.com>
2021-08-17 19:15:05 -0300

    bkp

---
+++ src/extras/linggo_db.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 20, 2021 at 10:44 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `linggo_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `ID_USUARIO` int(11) NOT NULL,
+  `APELIDO` varchar(30) COLLATE utf8_bin NOT NULL,
+  `EMAIL` varchar(50) COLLATE utf8_bin NOT NULL,
+  `SENHA` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios_rotinas`
+--
+
+DROP TABLE IF EXISTS `usuarios_rotinas`;
+CREATE TABLE IF NOT EXISTS `usuarios_rotinas` (
+  `ID_USUARIO` int(11) NOT NULL,
+  `ID_ROTINA` int(11) NOT NULL,
+  `TITULO_ROTINA` varchar(36) COLLATE utf8_bin NOT NULL,
+  `ROTINA_COR` varchar(12) COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
